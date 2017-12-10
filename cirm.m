665e0d07bd5f60475c01ecf200d8ea1319795207
César Pastorini <cesar.pastorini@gmail.com>
2017-12-10 12:57:36 -0200

    Código alternativo para simulação com canal MIMO perfeito
    O resultado é (e deve) ser igual ao de um BPSK sobre um AWGN.
    Vide comentários em cirm.m sobre como chaver entre o canal
    perfeito e o canal real simulado.

--- cirm.m
+++ cirm.m
@@ -1,8 +1,20 @@
+%%
+% Cria uma matrix função de transferência do canal.
+%
 function [ H ] = cirm( N, M )
-    % Tenta emular um canal, precisa de melhor embasamento.
-    seed = randi(10);
     
-    H = sqrt(seed)*randn(N, M) + 1i*sqrt(seed)*randn(N, M);
+    seed = randi(10);
+    H = seed*randn(N, M) + seed*1i*randn(N, M);
+    
+    %%
+    % COMENTE O CÓDIGO ACIMA (e descomente a linha abaixo :) para testar a
+    % simulação sobre um canal AWGN "perfeito".
+    %
+    % (O canal não é perfeito, há um pequêno ruído para fazer com que as
+    % colunas tenham uma norma diferente entre si. Com isso, a função
+    % sort_m() funciona de forma esperada para ordenar as colunas dessa
+    % matriz H)
+%     H = eye(N,M) + rand(N,M)/50;
 
 end
 
